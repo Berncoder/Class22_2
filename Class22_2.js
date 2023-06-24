a865a4228e85f989c01d454b1346330a62dc85a7
Bernadette <bernadettemetran35@gmail.com>
2023-06-23 19:46:07 -0700

    first commit

---
+++ Class22_2.js
@@ -0,0 +1,18 @@
+//create an array of numbers. sum all of the numbers. alert the sum
+let nums = [42,19,77,33,57]
+
+let sum = nums.reduce( (acc,c) => acc + c, 0 )
+
+alert(sum)
+
+
+//create a function that takes in an array of numbers
+//return a new array of numbers that is every original number squared
+
+
+//create a function that takes string
+//print the reverse of that string to the console
+
+//create a function that takes in a string
+//alert if the string is a palindrome or not
+
